726a82f68d66d0bf2517091bfc099188d65bf56c
Eiemmir <eiemmir@mail.ru>
2026-09-10 21:33:56 +0300

    changes

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const faqItems = document.querySelectorAll(".faq-item");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const faqItems = document.querySelectorAll(".faq-item");
 
-  faqItems.forEach((item) => {
-    const question = item.querySelector(".faq-question");
-    const answer = item.querySelector(".faq-answer");
+//   faqItems.forEach((item) => {
+//     const question = item.querySelector(".faq-question");
+//     const answer = item.querySelector(".faq-answer");
 
-    question.addEventListener("click", function () {
-      const isActive = item.classList.contains("active");
+//     question.addEventListener("click", function () {
+//       const isActive = item.classList.contains("active");
 
-      faqItems.forEach((otherItem) => {
-        if (otherItem !== item && otherItem.classList.contains("active")) {
-          otherItem.classList.remove("active");
-          const otherAnswer = otherItem.querySelector(".faq-answer");
-          otherAnswer.style.maxHeight = "0px";
-        }
-      });
+//       faqItems.forEach((otherItem) => {
+//         if (otherItem !== item && otherItem.classList.contains("active")) {
+//           otherItem.classList.remove("active");
+//           const otherAnswer = otherItem.querySelector(".faq-answer");
+//           otherAnswer.style.maxHeight = "0px";
+//         }
+//       });
 
-      item.classList.toggle("active");
+//       item.classList.toggle("active");
 
-      if (isActive) {
-        answer.style.maxHeight = "0px";
-      } else {
-        requestAnimationFrame(() => {
-          answer.style.maxHeight = answer.scrollHeight + "px";
-        });
-      }
-    });
-  });
-});
+//       if (isActive) {
+//         answer.style.maxHeight = "0px";
+//       } else {
+//         requestAnimationFrame(() => {
+//           answer.style.maxHeight = answer.scrollHeight + "px";
+//         });
+//       }
+//     });
+//   });
+// });
